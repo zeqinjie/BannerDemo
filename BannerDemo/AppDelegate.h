ab45a143e04a08b42371d355cf4b4520e5c40465
zeqinjie <1875193628@qq.com>
2015-05-27 17:44:56 +0800

    Initial Commit

---
+++ BannerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BannerDemo
+//
+//  Created by zhengzeqin on 15/5/27.
+//  Copyright (c) 2015年 com.injoinow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BannerDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  BannerDemo
+//
+//  Created by zhengzeqin on 15/5/27.
+//  Copyright (c) 2015年 com.injoinow. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
